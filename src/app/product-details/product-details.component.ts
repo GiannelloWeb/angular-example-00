3aa162a4a280aad4e9796a4369a59d7bb2a2af96
giannello2903 <giannello24@gmail.com>
2021-01-01 22:28:24 +0000

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -11,45 +11,49 @@ import { CartService } from "../cart.service";
 export class ProductDetailsComponent implements OnInit {
   product;
   options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-coords = [{
-  lat: 0,
-  lon: 0,
-  time: 0
-}];
-err = {
-  msg: '',
-  status: false
-}
-watcher = 0;
-getLocation(){
-  this.watcher = window.navigator.geolocation.watchPosition((position)=>{
-    this.coords.push({
-      lat: position.coords.latitude,
-      lon: position.coords.longitude,
-      time: position.timestamp
-    });
-    this.err={
-      msg: '',
-      status: false
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  coords = [
+    {
+      lat: 0,
+      lon: 0,
+      time: 0
     }
-
-  }, (err)=>{
-    this.err = {
-      msg: err.message,
-      status: true
-    }
-
-  }, this.options)
-}
+  ];
+  err = {
+    msg: "",
+    status: false
+  };
+  watcher = 0;
+  getLocation() {
+    this.watcher = window.navigator.geolocation.watchPosition(
+      position => {
+        this.coords.push({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+          time: position.timestamp
+        });
+        this.err = {
+          msg: "",
+          status: false
+        };
+      },
+      err => {
+        this.err = {
+          msg: err.message,
+          status: true
+        };
+      },
+      this.options
+    );
+  }
 
   addToCart(product) {
     window.alert("Your " + product.name + " has been added to the cart!");
     this.cartService.addToCart(product);
-    window.navigator.vibrate([300,200,100,100,100,100,100,100,300]);
+    window.navigator.vibrate([300, 200, 100, 100, 100, 100, 100, 100, 300]);
   }
 
   constructor(
