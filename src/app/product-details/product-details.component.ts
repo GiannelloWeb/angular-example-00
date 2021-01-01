64c26a29bbd6ed21e4427e247326cec35a7ed723
giannello2903 <giannello24@gmail.com>
2021-01-01 23:05:33 +0000

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -12,8 +12,8 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
   product;
   options = {
     enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
+    timeout: 15000,
+    maximumAge: 5000
   };
   coords = [
     {
