edcf0f4d9df3b51a9920ad0dd82bf85d840ba3be
giannello2903 <giannello24@gmail.com>
2021-03-08 02:11:02 +0000

    3 files modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
-import { MetasComponent } from './metas/metasComponent';
-import { HeadComponent } from './head/head.component';
+
 
 @NgModule({
   imports: [
@@ -35,8 +34,7 @@ import { HeadComponent } from './head/head.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
-    MetasComponent,
-    HeadComponent
+
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
